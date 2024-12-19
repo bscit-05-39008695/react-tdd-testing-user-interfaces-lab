d1028bb88ee70ddc3da302a3658d77355de389b6
bscit-05-39008695 <aronmutai99@gmail.com>
2024-12-19 16:51:17 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 function App() {
-  return <div>Your code here</div>;
-}
 
+    return (
+      <div>
+        <h1>Hi, I'm ARON MUTAI</h1>
+        <img src="runa.jpeg" alt="lol" width={100} height={80}/>
+        <h2>About Me</h2>
+        <p>Full stack developer with 1 year of professional experience specializing in modern web development technologies. Proficient in building end-to-end applications using JavaScript, React, and Python. Skilled at developing responsive front-end interfaces and implementing server-side functionality, demonstrating strong abilities in both UI/UX and back-end architecture.</p>
+        <ul>
+          <li>
+            <a href="https://github.com/bscit-05-39008695" target="_blank" rel="noopener noreferrer">
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/aron-mutai-3544712a6?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  };
 export default App;
